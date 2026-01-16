7620d074ee0dd40b380ee2d9bcc779a162cd6aff
Ryan <eklesp@icloud.com>
2026-01-15 16:37:28 -0800

    url adjust

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -105,14 +105,14 @@ export const externalPlayerOptions: ExternalPlayerOption[] = [
   {
     id: 'cineby.gd',
     name: 'Cineby.gd',
-    tvUrlTemplate: 'https://www.cineby.gd/tv/{TMDBID}/{season_num}/{episode_num}?play=true',
-    movieUrlTemplate: 'https://www.cineby.gd/movie/{TMDBID}?play=true',
+    tvUrlTemplate: 'https://www.cineby.gd/tv/{TMDBID}/{season_num}/{episode_num}',
+    movieUrlTemplate: 'https://www.cineby.gd/movie/{TMDBID}',
   },
   {
     id: 'bitcine.app',
     name: 'Bitcine.app',
-    tvUrlTemplate: 'https://www.bitcine.app/tv/{TMDBID}/{season_num}/{episode_num}?play=true',
-    movieUrlTemplate: 'https://www.bitcine.app/movie/{TMDBID}?play=true',
+    tvUrlTemplate: 'https://www.bitcine.app/tv/{TMDBID}/{season_num}/{episode_num}',
+    movieUrlTemplate: 'https://www.bitcine.app/movie/{TMDBID}',
   },
   // Add other external player options here in the future
 ];
